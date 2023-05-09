7dc267530d8c0e73f4b173ea835561824b0b7cc3
Krishna-Spexer <tyagikrish00@gmail.com>
2023-05-09 15:49:26 +0530

    More Parameters Of Print Function, Comments,Escape And Sequences

---
+++ More Parameters of Print, Comment, Escape and Sequences.py
@@ -0,0 +1,8 @@
+#Hey Krishna Avoid This Line As This Is A Comment
+print("This Python Project Shows More Parameters Of Print Function") #Also Include Comment,Escape And Sequence
+'''This Is The Function to add comments in multiple lines
+as this is line 2
+and now this is line 3'''
+print("hy",5,6) #Print More Than One Thing In Single Command
+print("hi",6,2, sep="_")
+print("hi","hello",end="krishna")
