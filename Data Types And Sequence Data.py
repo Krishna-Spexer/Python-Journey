e246460d7a97a414c43f20bd4820dbf4cf4b3c92
Krishna-Spexer <tyagikrish00@gmail.com>
2023-05-09 21:00:29 +0530

    Project For Data Types And Sequence Data

---
+++ Data Types And Sequence Data.py
@@ -0,0 +1,23 @@
+a=1
+b=True
+c="Hurry"
+d=None
+print("The Type Of a is",type(a))
+print("The Type Of b is",type(b))
+print("The Type Of c is",type(c))
+print("The Type Of d is",type(d))
+'''these were the data types and how to check them'''
+
+#These Are The Sequence Data Which Consist Of List And Tuple.
+
+list1=[1,23,"list1",[1,2,3]]
+print(list1)#this is a list
+
+tuple1=(1,43,56,"this is a tuple")
+print(tuple1)#this is a tuple
+
+'''Now We Will Check Mapped Data Also Known As Dictionaty'''
+print("Now Lets See The Mapped Data Type Which Is A Dictionary")
+
+dict1={"Name": "Krishna","Age": "16","CanVote": "False"}
+print(dict1)
