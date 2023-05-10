3bfe3a15a0eb5842e11045ab8144bd820ac11939
Krishna-Spexer <tyagikrish00@gmail.com>
2023-05-10 20:30:47 +0530

    Created My 1st Calculator (Simple)

---
+++ Creating A Simple Calculator.py
@@ -0,0 +1,23 @@
+#My First Try To Make A 2 Variable Simple Calculator
+
+n=str(input("Enter Your Good Name -"))
+num1=(float(input("Enter The 1st Number -")))
+num2=(float(input("Enter The 2nd Number -")))
+add= num1 + num2
+print("The Sum Of Number 1 And Number 2 is -",add)
+sub=num1 - num2
+print("The Difference Of Number 1 And Number 2 is -",sub)
+Mult= num1 * num2
+print("The Product Of Number 1 And Number 2 is -",Mult)
+div= num1/num2
+print("The Division Of Number 1 And Number 2 is -",div)
+floor= num1//num2
+print("The Floor Division Of Number 1 And Number 2 is -",floor)
+expo= num1**num2
+print("The Value Of Number 1 Raise To The Power Number 2 is -",expo)
+modu= num1%num2
+print("The Modulus Of Number 1 And Number 2 is -",modu)
+#All Arithmetic Operators Are Used In 2 variable
+print("Hope You Enjoyed Are \"All Arithmetic Calcutor\" In 2 Variable")
+feedback=(input("Please Do Give Us FeedBack Here -"))
+print("Thanks You For Using Our Service \n See You Soon",n) #used \n to switch on a new line
