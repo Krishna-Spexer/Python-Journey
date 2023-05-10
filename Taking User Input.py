9a4867bbb208d903fb9a740ff39281a7735ac8bf
Krishna-Spexer <tyagikrish00@gmail.com>
2023-05-10 22:57:01 +0530

    How To Take User Input

---
+++ Taking User Input.py
@@ -0,0 +1,7 @@
+a = input("Enter Your Name -")
+print("hy",a)
+
+b=int(input("Enter Any Number As Integer -"))
+c=int(input("Enter Any Number 2 As Integer -"))
+print(float(c)+(b))
+input(str("You Can Only Enter Any Word -"))
