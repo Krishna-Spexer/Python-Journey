96464909272635f5a9308bc858a14f55e2a27b00
Krishna-Spexer <tyagikrish00@gmail.com>
2023-05-11 22:41:41 +0530

    Slicing And Operation Of String And (More About Strings)

---
+++ String Slicing & Operation In Python.py
@@ -0,0 +1,10 @@
+a =(input("ENTER ANY STRING -"))
+print(type(a))
+alen = len(a)
+print(alen)#length of a
+print(a[0])#1st letter of your input(str)
+print(a[0:4])#0 is included and 4 es excluded
+print(a[:4])#python takes 0 as default here
+print(a[-5:-2])#print(a[len(a)-5:len(a)-2])
+print("here we have used slicing of strings and operated them as per our desired result")
+
---
+++ Strings In Python.py
@@ -0,0 +1,21 @@
+#Using Strings
+a="Krishna Here Learning Python"
+print(a[0])
+print("now let's try multiple line string\n")
+b='''this is a multiple line string
+
+hy bro how are you
+im fine
+are you having fun learning python
+im having lot of fun
+'''
+print(b[11])
+
+print('Now Lets Try Loops In Printing characters Of A string')
+
+for character in a:
+    print(character)
+
+print('Now Lets Try It With Our Multiple String "B"')
+for character in b:
+    print(character)
