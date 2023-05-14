f89a1db0b44a38a98dba320441be0deef4c4f03d
Krishna-Spexer <tyagikrish00@gmail.com>
2023-05-14 18:13:01 +0530

    String Methods Are In This Project (But Errors Will Be Fix Soon)

---
+++ String Methods.py
@@ -0,0 +1,24 @@
+a = "  Hy My name Is !! Krishna TYAGi !!  "
+print(a.upper())  # Converts All Characters In UPPER CASE
+print(a.lower())  # Converts All Characters In LOWER CASE
+print(a.strip())  # Removes White Spaces From From Starting and ending
+ # ERROR print(a,rstrip !())# Removes The Trailing Character Given 
+ # print(a.replace("krishna","Kishu"))
+print(a.split()) # Splits The String In Lists
+print(a.capitalize()) #turns 1s character to the upper case #Not Working ???
+print(a.center(60)) # This Aligns String to the center as per the Perimeter is Given
+ #print(a.count()) ERROR
+print(a.endswith("!"))
+ #print(a.find(i)) 
+print(a.index('n')) # shows 8 as n is shown at the 8 character
+print(a.isalnum()) # shows true if string consist only of A-Z, a-z ,0-9 (otherwise shows false)
+print(a.isalpha()) # shows true if the string only consist A-Z , a-z (Otherwise false)
+print(a.islower()) # checks if all the characters in the strings are in lower case
+print(a.isupper()) # checks if all the characters in the strings are in Upper case
+print(a.isprintable()) #checks that all the characters in this list are printable (like \n is not printable)
+print(a.isspace()) # returns true if the string only contain white spaces
+print(a.istitle()) # returns true if the 1st letter of your Word Is Capitzalize
+print(a.swapcase())#swap upper to lower case
+print(a.title())   #capitalize each letter of word within the string
+print("Now We Have Checked The String Methods, But As Of Now There Are Many Errors In This Which Will Be Solved Soon")
+ 
